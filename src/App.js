d8cc73f229b8534aac6d767253fa77a142da5376
jason <2B3>
2021-08-07 02:49:04 -0400

    commit 1: get previous state value

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const prevNameRef = useRef("nothing");
+  useEffect(() => {
+    prevNameRef.current = name;
+  }, [name]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <h3>My name is currently: [{name}]</h3>
+      <h3>My name was [{prevNameRef.current}] directly before that</h3>
+    </>
   );
 }
 
