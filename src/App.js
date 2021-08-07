00e6d15cd6c41e0dc749f998f8e89b9e24a49121
jason <2B3>
2021-08-07 03:34:27 -0400

    commit 2: conditional coding with refs

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const prevNameRef = useRef("nothing");
-  useEffect(() => {
-    prevNameRef.current = name;
-  }, [name]);
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+
+  const submitHandler = function (e) {
+    e.preventDefault();
+    let firstNameInput = firstNameRef.current.value;
+    let lastNameInput = lastNameRef.current.value;
+    console.log(`first name input is... ${firstNameInput}`);
+    console.log(`last name input is ... ${lastNameInput}`);
+    // Clear the input fields 
+    firstNameRef.current.value = "";
+    lastNameRef.current.value = "";
+  };
   return (
     <>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <h3>My name is currently: [{name}]</h3>
-      <h3>My name was [{prevNameRef.current}] directly before that</h3>
+      <input type="text" ref={firstNameRef} />
+      <input type="text" ref={lastNameRef} />
+      <button onClick={submitHandler}>Submit</button>
     </>
   );
 }
